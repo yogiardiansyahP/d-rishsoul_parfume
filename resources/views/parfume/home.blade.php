c2376a5e22e7cd89023ce68cc0e05b86c2efc386
yogiardiansyahP <yogiarddev@gmail.com>
2026-05-01 10:57:33 +0700

    Initial commit

---
+++ resources/views/parfume/home.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D'rish parfume</title>
+    <link rel="stylesheet" href="/asset/style.css">
+</head>
+<body>
+
+    <div class="navbar">
+        <div class="logo">
+            <h4>D'RISH SOUL</h4>
+            <p>JAKARTA . EST . 2025</p>
+        </div>
+        <div class="navbar_content">
+            <div class="navbar_content1">KOLEKSI</div>
+            <div class="navbar_content1">BACKGROUND</div>
+            <div class="navbar_content1">AROMA</div>
+            <div class="navbar_content1">FILOSOFI</div>
+        </div>
+    </div>
+
+
+    <section class="hero">
+
+    <!-- LEFT CONTENT -->
+        <div class="hero-left">
+            <h1>DI MANA</h1>
+            <h2>AROMA</h2>
+            <h1>MENJADI</h1>
+            <h1>KARYA SENI</h1>
+
+            <p>
+                "Lebih dari sekadar parfum, Drish Perfume adalah
+                identitas bagi mereka yang ingin meninggalkan kesan
+                istimewa di setiap langkah."
+            </p>
+
+            <a href="#" class="btn">EKSPLORASI KOLEKSI →</a>
+
+            <div class="stats">
+                <div class="item">
+                    <h3>10</h3>
+                    <span>Tahun Pengalaman</span>
+                </div>
+
+                <div class="item">
+                    <h3>1</h3>
+                    <span>Tahun Berdiri</span>
+                </div>
+
+                <div class="item">
+                    <h3>9</h3>
+                    <span>Bahan Berkualitas</span>
+                </div>
+            </div>
+        </div>
+
+        <!-- RIGHT IMAGE -->
+        <div class="hero-right">
+            <img src="/asset/image/polos.png" alt="Parfume">
+        </div>
+
+    </section>
+    
+</body>
+</html>
